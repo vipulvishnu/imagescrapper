8152e81b82ad6bff77c58a9bd7651003def4129d
ciciluke <cici.luke@sayonetech.com>
2015-11-13 19:16:19 +0530

    Updated Images Spider

--- imagescrapper/spiders/images_spider.py
+++ imagescrapper/spiders/images_spider.py
@@ -1,35 +1,46 @@
 import scrapy
 
 from lxml import html
+from urlparse import urlparse
 from scrapy.http import Request
 from imagescrapper.items import ImagescrapperItem
 
 
 class ImagescrapperSpider(scrapy.Spider):
- name = "pixabay"
- domain = "https://pixabay.com/"
- start_urls = [
-      "https://pixabay.com/"
+  name = "pixabay"
+
+  start_urls = [
+      "https://pixabay.com/en/photos/?order=best&cat=feelings"
   ]
 
- def parse(self, response):
-   doc = html.fromstring(response.body)
-   item = ImagescrapperItem()
-   img_urls = []
-   image_urls1 = doc.xpath('.//img/@src')
-   image_urls2 = doc.xpath('.//img/@data-src')
-   image_urls = image_urls1 + image_urls2
-   for image_url in image_urls:
-    if "https://" not in image_url:
-      img_urls.append(self.domain + image_url)
-    else:
-      img_urls.append(image_url)
-   item['image_url'] = img_urls
-   yield item
+  def parse(self, response):
+    parsed_uri = urlparse(response.url)
+    domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+    doc = html.fromstring(response.body)
+    item = ImagescrapperItem()
+    img_urls = self.get_images(doc, domain)
+    item['image_url'] = img_urls
+    yield item
 
-   # Finding next urls
-   next_urls = doc.xpath('.//a/@href')
-   for next_url in next_urls:
-    if "https://" not in next_url:
-      next_url = self.domain + next_url
+    # Get the next urls
+    next_urls = doc.xpath('.//a/@href')
+    for url in next_urls:
+      next_url = self.get_urls_logic(url, domain)
       yield Request(url=next_url)
+
+  def get_urls_logic(self, url, domain):
+    if 'http' not in url:
+      url = domain + url
+      return url
+    else:
+      return url
+
+  def get_images(self, doc, domain):
+    img_urls = []
+    image_urls1 = doc.xpath('.//img/@src')
+    image_urls2 = doc.xpath('.//img/@data-src')
+    image_urls = image_urls1 + image_urls2
+    for image_url in image_urls:
+      if 'blank' not in image_url:
+        img_urls.append(self.get_urls_logic(image_url, domain))
+    return img_urls
